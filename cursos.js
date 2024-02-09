f251aaa5897127078589e3a59464327e596d0dd9
walquiria <159277587+walkiria85@users.noreply.github.com>
2024-02-09 09:47:53 -0600

    Add files via upload

---
+++ cursos.js
@@ -0,0 +1,45 @@
+let infoCursos = {
+    'programacion ' :[
+        {
+
+            id:1,
+            titulo:'Aprende Python',
+            lenguajes: 'python',
+            vistas: 1500,
+            nivel: 'basico'
+        },
+        {
+            id:2,
+            titulo:' Python  Intermedio',
+            lenguajes: 'python',
+            vistas: 13553,
+            nivel: 'intermedio'
+        },
+        {
+            id:3,
+            titulo:'Aprende JavaScript',
+            lenguajes: 'JavaScript',
+            vistas: 102223,
+            nivel: 'basico'
+        }
+    ],
+    'matematicas':[
+        {
+            id:1,
+            titulo:'Aprende Algebra',
+            lenguajes: 'algebra',
+            vistas: 15427,
+            nivel: 'intermedio'
+        },
+        {
+            id:2,
+            titulo:'Aprende Algebra',
+            lenguajes: 'algebra',
+            vistas: 102223,
+            nivel: 'basico'
+        }
+
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
